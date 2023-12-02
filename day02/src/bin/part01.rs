6e5b4c285d6fa5b5a870960d138ba1cd4dea9067
jeffock <jeffock1656@gmail.com>
2023-12-01 23:43:12 -0600

    day02 init commit

---
+++ day02/src/bin/part01.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("{}", "day02 part1");
+}
+
+ 
---
+++ day02/target/debug/part01.d
@@ -0,0 +1,1 @@
+C:\Users\jeffo\Coding\advent_of_code_RUST\day02\target\debug\part01.exe: C:\Users\jeffo\Coding\advent_of_code_RUST\day02\src\bin\part01.rs
